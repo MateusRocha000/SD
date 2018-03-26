6dc74460003c610426afea8aa5adebc45a9c4e69
felipefreitas <felipesafreitas@gmail.com>
2018-03-26 12:41:48 -0300

    changed some things

--- questao01/iterativo/server.c
+++ questao01/iterativo/server.c
@@ -15,37 +15,43 @@
 int main(int argc, char *argv[])
 
 {  
-  int  sockfd, newsockfd, clilen;
+  int  sockfd, newsockfd, clilen, n;
   struct sockaddr_in  cli_addr, serv_addr;
 
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)   /* abre um socket TCP */
-    erro("abrindo socket\n")
+  erro("abrindo socket\n")
 
   /* Liga o processo servidor ao seu endereço local */
 
   bzero((char *)&serv_addr, sizeof(serv_addr));
 
   serv_addr.sin_family = AF_INET;
-
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
   serv_addr.sin_port = htons(SERV_TCP_PORT);
 
   if  (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)       erro("binding\n");
 
   listen(sockfd, 5);
-
-  clilen = sizeof(cli_addr);
-  printf("I'm %i Listening %i\n", getpid(), sockfd); 
-  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-  if (newsockfd < 0)
-        erro("server: accept error")
+  
+  while(1){
+    clilen = sizeof(cli_addr);
+    printf("I'm %i Listening %i\n", getpid(), sockfd); 
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0)
+      erro("server: accept error")
+    char buf[MAX_LINE];
     
-   char buf[MAX_LINE];
-   recv(newsockfd, buf, MAX_LINE, 0);
-   sleep(5);          
-   send(newsockfd, buf, strlen(buf) + 1, 0); /* process the request */
+    while(1){
+      n = recv(newsockfd, buf, MAX_LINE, 0);
+      if (n == 0) {
+        close(newsockfd);       
+        break;
+      }
+      printf("%s\n", buf);
+      sleep(5);       
+      send(newsockfd, buf, strlen(buf) + 1, 0); /* process the request */
+    }
+  }
 
-    close(newsockfd);       
   return 0;
 }
--- questao01/server.c
+++ questao01/server.c
@@ -35,21 +35,22 @@ int main(int argc, char *argv[])
   if  (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)       erro("binding\n");
 
   listen(sockfd, 5);
+  clilen = sizeof(cli_addr);
+  printf("I'm %i Listening %i\n", getpid(), sockfd); 
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-  
-      clilen = sizeof(cli_addr);
-      printf("I'm %i Listening %i\n", getpid(), sockfd); 
-      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-      if (newsockfd < 0)
-            erro("server: accept error")
+  if (newsockfd < 0)
+        erro("server: accept error")
+
+  char buf[MAX_LINE];
   while(1)
   {      
-       char buf[MAX_LINE];
-       recv(newsockfd, buf, MAX_LINE, 0);
-       sleep(5);          
-       send(newsockfd, buf, strlen(buf) + 1, 0); /* process the request */
-
-        close(newsockfd); 
+    recv(newsockfd, buf, MAX_LINE, 0);
+    printf("BYTES RECEBIDOS\n");
+    sleep(5);          
+    printf("%s\n", buf);
+    send(newsockfd, buf, strlen(buf) + 1, 0); /* process the request */
+    close(newsockfd); 
   }      
   return 0;
 }
